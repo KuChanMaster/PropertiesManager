a01f95af1e8c43657f14d94dd42449a0f3d3ec92
KuChanMaster <liushuaibzinga@outlook.com>
2024-12-04 19:37:04 +0800

    业主模块设计与实现

---
+++ springboot/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,39 @@
+package com.example.mapper;
+
+import com.example.entity.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 操作user相关数据接口
+*/
+public interface UserMapper {
+
+    /**
+      * 新增
+    */
+    int insert(User user);
+
+    /**
+      * 删除
+    */
+    int deleteById(Integer id);
+
+    /**
+      * 修改
+    */
+    int updateById(User user);
+
+    /**
+      * 根据用户姓名查询
+     */
+    @Select("select * from user where username = #{username}")
+    User selectByUserName(String username);
+
+    /**
+      * 查询所有
+    */
+    List<User> selectAll(User user);
+
+}
---
+++ springboot/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,60 @@
+//package com.example.service;
+//
+//import cn.hutool.core.util.ObjectUtil;
+//import com.example.common.Constants;
+//import com.example.common.enums.ResultCodeEnum;
+//import com.example.entity.User;
+//import com.example.exception.CustomException;
+//import com.example.mapper.UserMapper;
+//import com.github.pagehelper.PageHelper;
+//import com.github.pagehelper.PageInfo;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//
+//@Service
+//public class UserService {
+//
+//    @Resource
+//    private UserMapper userMapper;
+//
+//
+//    public void add(User user) {
+//        // 从数据库中查出是否有相同用户名的业主
+//        User dbUser = userMapper.selectByUserName(user.getUsername());
+//        if (ObjectUtil.isNotEmpty(dbUser)) {
+//            throw new CustomException(ResultCodeEnum.USER_EXIST_ERROR);
+//        }
+//        // 如果没有密码，给他个默认密码
+//        if (ObjectUtil.isEmpty(user.getPassword())) {
+//            user.setPassword(Constants.USER_DEFAULT_PASSWORD);
+//        }
+//        // 如果没有角色， 给他个业主的角色
+//        if (ObjectUtil.isEmpty(user.getRole())) {
+//            user.setRole(RoleEnum.USER.name());
+//        }
+//        // 往user表里新增一条业主信息
+//        userMapper.insert(user);
+//    }
+//
+//    public PageInfo<User> selectPage(User user, Integer pageNum, Integer pageSize) {
+//        PageHelper.startPage(pageNum, pageSize);
+//        List<User> list = userMapper.selectAll(user);
+//        return PageInfo.of(list);
+//    }
+//
+//    public void updateById(User user) {
+//        userMapper.updateById(user);
+//    }
+//
+//    public void deleteById(Integer id) {
+//        userMapper.deleteById(id);
+//    }
+//
+//    public void deleteBatch(List<Integer> ids) {
+//        for (Integer id : ids) {
+//            userMapper.deleteById(id);
+//        }
+//    }
+//}
